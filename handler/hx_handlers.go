e74ae8d771179bfcba2e29324c8f18d4e1bf22ff
abdealijaroli <abdealiburu03@gmail.com>
2024-02-27 09:26:59 +0530

    Refactor CelebHandler function to handle htmx requests and generate AI response

--- handler/hx_handlers.go
+++ handler/hx_handlers.go
@@ -3,29 +3,27 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/abdealijaroli/gostream/util"
 )
 
 func CelebHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Header.Get("HX-Request") == "true" {
-		fmt.Println("This is an htmx request")
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Failed to parse form", http.StatusBadRequest)
+			return
+		}
 	}
-
-	fmt.Println("Request method:", r.Method)
-
-	err := r.ParseForm()
-	if err != nil {
-		http.Error(w, "Failed to parse form", http.StatusBadRequest)
-		return
-	}
-
-	fmt.Println("Form data:", r.Form)
 
 	celeb := r.PostFormValue("celeb")
-	if celeb == "" {
+	if celeb != "" {
+		fmt.Println("Celeb:", celeb)
+		resp := util.GenerateAIResponse(celeb)
+		fmt.Fprint(w, resp)
+	} else {
 		fmt.Fprintf(w, "No name provided!")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-
-	fmt.Println("Celeb:", celeb)
 }
