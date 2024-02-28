c04b2fd7f20b38e0c9693c5aefa34ffd85ffbef3
abdealijaroli <abdealiburu03@gmail.com>
2024-02-28 19:23:43 +0530

    upd: godotenv package and handle error in main.go, util/genai.go for generating AI response

--- cmd/main.go
+++ cmd/main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"net/http"
 
+	"github.com/joho/godotenv"
+
 	"github.com/abdealijaroli/gostream/handler"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
 	fs := http.FileServer(http.Dir("./view"))
 	http.Handle("/", fs)
 
 	http.HandleFunc("/celeb", handler.CelebHandler)
 
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
 	}
-	
 }
